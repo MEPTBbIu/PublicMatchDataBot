e52ea4c5af75148b06510742b21f142c9770577a
Howard Chung <howardzchung@gmail.com>
2018-03-10 06:10:10 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// Load up the discord.js library
+const Discord = require("discord.js");
+
+const self = ["my", "i"];
+const negatives = ["don't", "can't", "doesn't", "aren't"];
+const matches = ["matches", "games", "stats"];
+
+// This is your client. Some people call it `bot`, some people call it `self`, 
+// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
+// this is what we're refering to. Your client.
+const client = new Discord.Client();
+
+// Here we load the config.json file that contains our token and our prefix values. 
+const config = require("./config.json");
+// config.token contains the bot's token
+// config.prefix contains the message prefix.
+
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+  if(self.some(s => message.content.includes(s)) && negatives.some(negative => message.content.includes(negative)) && matches.some(match => message.content.includes(match))) {
+    console.log(message.content, 'replying');
+    message.reply('have you enabled Expose Public Match Data in the Dota 2 client?');
+  } else {
+    console.log(message.content, 'not replying');
+  }
+});
+
+client.login(config.token);
